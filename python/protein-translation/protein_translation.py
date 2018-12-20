95415505b7054ab0116febc04723a9b6ccbac7a7
Patrick Barringer <patrick.barringer@gmail.com>
2018-12-20 13:28:24 -0500

    Completed protein-translation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,32 @@
+CODON_DICT = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP',
+    'UAG': 'STOP',
+    'UGA': 'STOP'
+    }
+
+
 def proteins(strand):
-    pass
+    if len(strand) % 3 != 0:
+        raise ValueError("The strand must consist of 3 neucleotide codons")
+
+    codons = [strand[i: i+3] for i in range(0, len(strand), 3)]
+    prot = [CODON_DICT[codon] for codon in codons]
+
+    if 'STOP' in prot:
+        prot = prot[0: prot.index('STOP')]
+
+    return prot
